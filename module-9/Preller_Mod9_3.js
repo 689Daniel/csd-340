048f9173d29b1518be0d661d4c2b63e0ddd1fa61
Daniel Preller <689daniel@gmail.com>
2025-05-06 16:38:23 -0400

    Added assignment 9.3 and updated index.html

---
+++ module-9/Preller_Mod9_3.js
@@ -0,0 +1,20 @@
+let firstValue = "farming";
+let secondValue = "atmosphere";
+let thirdValue = "Fall";
+let fourthValue = "plans"
+
+function setFirstValue() {
+	document.getElementById("firstValue").innerHTML = firstValue;
+}
+
+function setSecondValue() {
+	document.getElementById("secondValue").innerHTML = secondValue;
+}
+
+function setThirdValue() {
+	document.getElementById("thirdValue").innerHTML = thirdValue;
+}
+
+function setFourthValue() {
+	document.getElementById("fourthValue").innerHTML = fourthValue;
+}
